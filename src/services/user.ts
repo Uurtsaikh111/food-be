d737b7114b2d6ed703c39d960bb46261b21fce85
Uurtsaikh <uuree.i4uuree@gmail.com>
2024-04-02 00:06:54 +0800

    home

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,1 +1,17 @@
-var jwt = require('jsonwebtoken');
+import jwt from "jsonwebtoken";
+// import { generateJwtToken } from "../utils/generate-token";
+
+export const loginService = async (email: string, password: string) => {
+  if (email == "admin@gmail.com" && password == "admin") {
+    const userInfo = {
+      email: email,
+      name: "John Doe",
+    };
+    const newToken = jwt.sign(userInfo, "my-super-duper-secret-key", {
+      expiresIn: "1h",
+    });
+    return newToken;
+  } else {
+    throw new Error("Invalid credentials");
+  }
+};
