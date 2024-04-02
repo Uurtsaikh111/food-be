b4a445f478b9bd13007bdcc6725b86eb01fb1644
Uurtsaikh <uuree.i4uuree@gmail.com>
2024-04-02 22:15:05 +0800

    home

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -5,8 +5,14 @@ import { connect } from "@/helper/db";
 connect();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.body?.firstName || !req.body?.lastName || !req.body?.email) {
+    res.status(400).json("firstName, lastName,email is missing");
+  }
+
+  const { firstName, lastName, age, email } = req.body;
+
   try {
-    const user = await createUser("firstName", "lastName", "email", 22);
+    const user = await createUser(firstName, lastName, email ,age);
     res.status(200).json({ message: "Sucsessfully user created", user });
   } catch (e: any) {
     return res.status(400).json({ message: e.message });
